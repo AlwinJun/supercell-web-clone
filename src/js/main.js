db450652417a492ea1ec8666ab2cd143cf1792f2
AJ <mahidlawonaljun@gmail.com>
2023-07-05 12:40:48 +0800

    Refactor code: Apply DRY and KISS Organize variables Remove unnecessary code and comments

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,3 @@
-// console.log('test');
-
 // DOM Elements
 const navBtn = document.querySelector('.nav-toggle');
 const navContent = document.querySelector('.nav-expand');
@@ -11,19 +9,24 @@ const carouselContainer = document.querySelector('.carousel-container');
 const cards = carouselContainer.querySelectorAll('.card');
 const prevBtn = document.querySelector('.carousel-prev');
 const nextBtn = document.querySelector('.carousel-next');
+const dotContainer = document.querySelector('.dot-container');
 
 //Get  styles property
-// :root
 const rootStyles = getComputedStyle(document.documentElement);
 const inactiveColor = rootStyles.getPropertyValue('--clr-inactive');
 const activeInactiveColor = rootStyles.getPropertyValue(
   '--clr-active-inactive'
 );
-// Cards
-const style = window.getComputedStyle(cards[0]);
-const marginRight = style.getPropertyValue('margin-right');
 
-// Event HAndlers
+// Carousel Variables
+const cardCount = cards.length;
+const style = window.getComputedStyle(cards[0]);
+const marginRight = parseInt(style.getPropertyValue('margin-right'));
+let currentCardIndex = 0;
+let currentDotIndex = 0;
+let dotButtons;
+
+// Helper Functions
 const resetNavContent = () => {
   navItems.forEach((item) => {
     item.classList.remove('click');
@@ -32,10 +35,33 @@ const resetNavContent = () => {
   });
 };
 
+const toggleNavContent = (navItem) => {
+  navItem.classList.toggle('click');
+  const navItemText = navItem.querySelector('h3');
+  const navItemArrowBtn = navItem.querySelector('span');
+  const isClicked = navItem.classList.contains('click');
+
+  // Add 'rotate' and 'white' if isClicked is truthy
+  navItemText.style.color = isClicked ? 'white' : inactiveColor;
+  navItemArrowBtn.classList.add('rotate', isClicked);
+};
+
+const updateCarousel = () => {
+  const cardWidth = carouselContainer.children[0].offsetWidth; //Get first card witdh
+  const translateX = currentCardIndex * (cardWidth + marginRight);
+  carouselContainer.style.transform = `translateX(-${translateX}px)`;
+};
+
+const updateActiveDotButton = (currentDotIndex) => {
+  dotButtons.forEach((btn, index) => {
+    btn.classList.toggle('active', index === currentDotIndex);
+  });
+};
+
+// Event Handlers
 const navBarExpand = () => {
   navBtn.classList.toggle('toggled');
   navContent.classList.toggle('expand');
-
   resetNavContent();
 
   navItems.forEach((item) => {
@@ -45,131 +71,71 @@ const navBarExpand = () => {
 
 const showSubItems = (e) => {
   const navItem = e.currentTarget;
-  const navItemText = navItem.querySelector('h3');
-  const navItemArrowBtn = navItem.querySelector('span');
-  const isClicked = navItem.classList.contains('click');
-
   resetNavContent();
-
-  if (!isClicked) {
-    // Toggle 'click' class on the clicked nav item
-    navItem.classList.toggle('click');
-    navItemText.style.color = 'white';
-    navItemArrowBtn.classList.add('rotate');
-  }
+  toggleNavContent(navItem);
 };
 
 const showSearchBar = () => {
   searchBar.classList.toggle('search');
 };
 
-// Carousel
-const cardCount = cards.length;
-let addCardMargin = parseInt(marginRight);
-let currentCardIndex = 0;
-let dotButtons;
-let currentDotIndex = 0;
-const dotContainer = document.querySelector('.dot-container');
-
-const intialCarouselStyle = () => {
-  // Prev btn initial state hidden
-  prevBtn.style.visibility = 'hidden';
-
-  if (window.innerWidth > 790) {
-    // Viewport medium-up
-    if (cards.length === 2) {
-      nextBtn.style.visibility = 'hidden';
-    }
-  } else {
-    // Viewport mobile
-    if (cards.length === 1) {
-      nextBtn.style.visibility = 'hidden';
-    }
-  }
-
-  displayDotButtons()
-    .then(dotButtonsSlider)
-    .catch((error) => console.log(error));
-};
-
-function dotButtonsSlider() {
-  // Add event listeners to dot buttons
-  dotButtons.forEach((button, index) => {
+const dotButtonsSlider = () => {
+  dotButtons.forEach((button, index, arr) => {
     button.addEventListener('click', () => {
+      // Calculate the currentCardIndex based on the dot button index and the number of cards to display
+      // 2 cards for viewport medium-up and 1 card for mobile view
       currentCardIndex = index * (window.innerWidth > 790 ? 2 : 1);
+
+      //Hide next/prev button when first or last button has been click
+      nextBtn.style.visibility =
+        index === arr.length - 1 ? 'hidden' : 'visible';
+      prevBtn.style.visibility = index > 0 ? 'visible' : 'hidden';
+
       currentDotIndex = index;
-      if (window.innerWidth > 790 ? 2 : 1)
-        updateActiveDotButton(currentDotIndex);
+      updateActiveDotButton(currentDotIndex);
       updateCarousel();
     });
   });
-}
+};
 
-function displayDotButtons() {
-  return new Promise((resolve, reject) => {
-    const buttons = Array.from(cards).map((_, index) => {
-      const button = document.createElement('button');
-      if (index === 0) {
-        button.classList.add('active');
-      }
-
-      return button;
-    });
-
-    if (window.innerWidth > 790) {
-      const buttonHalfLength = Math.ceil(cards.length / 2);
-      const desktopDotButton = buttons.slice(0, buttonHalfLength);
-      dotContainer.append(...desktopDotButton);
-    } else {
-      dotContainer.append(...buttons);
+const displayDotButtons = () => {
+  const buttons = Array.from(cards).map((_, index) => {
+    const button = document.createElement('button');
+    if (index === 0) {
+      button.classList.add('active');
     }
 
-    dotButtons = dotContainer.querySelectorAll('button');
-
-    if (dotButtons.length > 0) {
-      resolve();
-    } else {
-      reject('No buttons found');
-    }
+    return button;
   });
-}
 
-function updateActiveDotButton(currentDotIndex) {
-  dotButtons.forEach((btn, index) => {
-    if (index === currentDotIndex) {
-      btn.classList.add('active');
-    } else {
-      btn.classList.remove('active');
-    }
-  });
-}
+  if (window.innerWidth > 790) {
+    // For viewport medium-up get the half of the cards length and round it up
+    // Viewport meduim-up have 2 cards per screen.If the carousel has 9 card content it will  have a 5 slide
+    const buttonHalfLength = Math.ceil(cards.length / 2);
+    const desktopDotButton = buttons.slice(0, buttonHalfLength);
+    dotContainer.append(...desktopDotButton);
+  } else {
+    dotContainer.append(...buttons);
+  }
 
-const updateCarousel = () => {
-  const cardWidth = carouselContainer.children[0].offsetWidth;
-  const translateX = currentCardIndex * (cardWidth + addCardMargin);
-  carouselContainer.style.transform = `translateX(-${translateX}px)`;
+  dotButtons = dotContainer.querySelectorAll('button');
 };
 
 const previousButton = () => {
   nextBtn.style.visibility = 'visible';
 
   if (window.innerWidth > 790) {
-    // 2 card content / Viewport medium-up
     if (currentCardIndex >= 2) {
       currentCardIndex -= 2;
     }
   } else {
-    // 1 card / Viewport mobile
     if (currentCardIndex >= 1) {
       currentCardIndex -= 1;
     }
   }
 
-  // Hide previous button on first slide
-  if (currentCardIndex === 0) {
-    prevBtn.style.visibility = 'hidden';
-  }
-
+  // Hide button previous on first slide
+  prevBtn.style.visibility = currentCardIndex === 0 ? 'hidden' : 'visible';
   currentDotIndex--;
   updateActiveDotButton(currentDotIndex);
   updateCarousel();
@@ -179,26 +145,38 @@ const nextButton = () => {
   prevBtn.style.visibility = 'visible';
 
   if (window.innerWidth > 790) {
-    // 2 card content / Viewport medium-up
     if (currentCardIndex < cardCount - 2) {
       currentCardIndex += 2;
     }
   } else {
-    // 1 card / Viewport mobile
     if (currentCardIndex < cardCount - 1) {
       currentCardIndex += 1;
     }
   }
 
   // Hide next button on last slide
-  if (currentCardIndex >= cardCount - 1) {
-    nextBtn.style.visibility = 'hidden';
-  }
+  nextBtn.style.visibility =
+    currentCardIndex >= cardCount - 1 ? 'hidden' : 'visible';
 
   currentDotIndex++;
   updateActiveDotButton(currentDotIndex);
-
   updateCarousel();
+};
+
+const intialCarouselStyle = () => {
+  // Prev btn initial state hidden
+  prevBtn.style.visibility = 'hidden';
+
+  if (window.innerWidth > 790) {
+    // Viewport medium-up {
+    nextBtn.style.visibility = cards.length <= 2 ? 'hidden' : 'visible';
+  } else {
+    // Viewport mobile
+    nextBtn.style.visibility = cards.length === 1 ? 'hidden' : 'visible';
+  }
+
+  displayDotButtons();
+  dotButtonsSlider();
 };
 
 //Event Listeners
